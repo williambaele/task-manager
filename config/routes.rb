321cc0ac9ea410438433aa8fdf12dacae5da0f2c
William Baele <baelewilliam1999@gmail.com>
2022-12-18 10:16:53 +0800

    Devise work done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  
   # Defines the root path route ("/")
   # root "articles#index"
   resources :tasks
