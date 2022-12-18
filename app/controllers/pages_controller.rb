bd1a51aa5783bb201f2728609b96bae072fed3cc
William Baele <baelewilliam1999@gmail.com>
2022-12-18 15:38:15 +0800

    added dynamic counting urgent tasks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,15 @@ class PagesController < ApplicationController
   def index
     @tasks = Task.all
 
+
     @my_tasks = Task.all.order('updated_at DESC').select do |task|
       task.user_id == current_user.id
     end
+
+    today = Date.today
+    @urgent_tasks = @my_tasks.select do |task|
+      task.end_date <= today + 3.days
+    end
+    @urgent_tasks = @urgent_tasks.count
   end
 end
