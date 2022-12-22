c213cc51faf4ceac0fd44c530787199377ac92f7
William Baele <baelewilliam1999@gmail.com>
2022-12-22 09:15:57 +0800

    page all task created

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   patch "tasks/:id/mark_as_finished", to: "tasks#mark_as_finished", as: :mark_as_finished
   resources :tasks
-
+  get '/all-tasks', to: "pages#tasks"
   get '/new-task', to: "tasks#new"
 end
